1baef74ca36dd74ecffeb51fb07433e824058435
rmflawk <ant921111@naver.com>
2019-10-30 15:50:37 +0900

    hello

--- app/src/main/java/com/youngstudio/githubtest/MainActivity.java
+++ app/src/main/java/com/youngstudio/githubtest/MainActivity.java
@@ -3,12 +3,20 @@ package com.youngstudio.githubtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv= findViewById(R.id.tv);
+
+
     }
 }
